d21bd40117feb31e7734868265da7754dda7feba
Karas <karasgupta@gmail.com>
2025-11-29 15:10:59 +0800

    Ready for Vercel Deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // Alamat Backend Laravel Kamu
-const API_URL = 'http://127.0.0.1:8000/api';
-// const API_URL = 'https://abcd-123.ngrok-free.app/api';
+// const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'https://ferry-backend-production-73a4.up.railway.app/api';
 
 const api = axios.create({
     baseURL: API_URL,
